4e6055188304d1cf0ab2edcdcb706b7d2b252cec
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2023-05-20 22:21:19 +0300

    adding logic in component universalInputField

--- src/component/UniversalInput/UniversalInputField.tsx
+++ src/component/UniversalInput/UniversalInputField.tsx
@@ -1,32 +1,45 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
 import s from './UniversalInputField.module.css'
 
 export type UniversalInputFieldTypeProps = {
 callBack: (title: string)=> void
 };
-export const UniversalInputField: React.FC<UniversalInputFieldTypeProps> = ( props ) => {
-	const { callBack } = props
+export const UniversalInputField: React.FC<UniversalInputFieldTypeProps> = ( { callBack } ) => {
+	
 	const [title, setTitle] = useState('')
+	const [error, setError] = useState<string | null>(null)
 	const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
 		setTitle(e.currentTarget.value)
 	}
 	const onClickButtonHandler = () => {
-		callBack( title )
-		setTitle('')
+		if ( title.trim().length !== 0 ) {
+			callBack( title )
+			setTitle('')
+		} else {
+			setError('Title is required')
+			setTitle('')
+		}
 	}
-	
+	const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+		if ( error !== null ) {
+			setError(null)
+		}
+		if ( e.code === 'Enter' ) {
+			onClickButtonHandler()
+		}
+	}
 	return (
 		<div className={s.wrapper}>
 			<div className={s.textInputWrapper}>
 				<input value={title}
 				       onChange={onChangeInputHandler}
+				       onKeyDown={onKeyDownHandler}
 				       placeholder={"Type Here"}
 				       className={s.textInput}
 				/>
+				{<span style={{color: 'red'}}>{error}</span>}
 			</div>
 			<button className={s.btnInput} onClick={onClickButtonHandler}>+</button>
 		</div>
-		
-		
 	);
 };
